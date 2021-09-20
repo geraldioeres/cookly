73d8d4d2de1845f27c2423f04af5453b2a9e94f2
geraldiors <geralmers01@gmail.com>
2021-09-20 16:42:00 +0800

    Updated main.go
    Insert struct of feature

--- main.go
+++ main.go
@@ -1,13 +1,52 @@
 package main
 
-import "fmt"
+import "time"
+
+type Users struct {
+	ID         int
+	Name       string
+	Email      string
+	Password   string
+	Created_At time.Time
+	ModifiedAt time.Time
+}
+
+type Recipes struct {
+	ID           int
+	UserID       int
+	RecipeTypeID int
+	Name         string
+	Blurb        string
+	Rating       float32
+	Ingredients  []string
+	CreatedAt    time.Time
+	ModifiedAt   time.Time
+}
+
+type RecipeTypes struct {
+	ID         int
+	Name       string
+	CreatedAt  time.Time
+	ModifiedAt time.Time
+}
+
+type Collections struct {
+	UserID     int
+	RecipeID   int
+	CreatedAt  time.Time
+	ModifiedAt time.Time
+}
+
+type Reviews struct {
+	ID int
+	UserID int
+	RecipeID int
+	Rating int
+	Description string
+	CreatedAt time.Time
+	ModifiedAt time.Time
+}
 
 func main() {
-	fmt.Println("Welcome to simple calc")
-}
-func addition(a, b int) int {
-	return a + b
-}
-func subtraction(a, b int) int {
-	return a - b
+
 }
