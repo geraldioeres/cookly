dc2983fb1f08b104388f6a27faa707a404513b87
geraldiors <geralmers01@gmail.com>
2021-09-22 13:47:25 +0800

    added user controllers and base response

--- controllers/c_user.go
+++ controllers/c_user.go
@@ -1,5 +1,29 @@
 package controllers
 
-func UserLogin() {
+import (
+	users "cookly/models/Users"
+	"cookly/models/response"
+	"net/http"
 
+	"github.com/labstack/echo/v4"
+)
+
+func UserRegiterController(c echo.Context) error {
+	var UserRegister users.UserRegister
+	c.Bind(&UserRegister)
+
+	if UserRegister.Name == "" {
+		return c.JSON(http.StatusBadRequest, response.BaseResponse{
+			Code: http.StatusBadRequest,
+			Message: "Name cannot blank",
+			Data: nil,
+		})
+	}
+
+	var userDB users.Users
+	userDB.Name = UserRegister.Name
+	userDB.Email = UserRegister.Email
+	userDB.Password = UserRegister.Password
+
+	result := 
 }
