3c30e9e790544890c616b17f8fefbe6dde5291a0
geraldiors <geralmers01@gmail.com>
2021-09-23 23:52:48 +0800

    reset the project
    start over from the beginning

--- main.go
+++ main.go
@@ -1,13 +1,2 @@
 package main
 
-import "fmt"
-
-func main() {
-	fmt.Println("Welcome to simple calc")
-}
-func addition(a, b int) int {
-	return a + b
-}
-func subtraction(a, b int) int {
-	return a - b
-}
