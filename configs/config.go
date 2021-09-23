330bbcaa1fa36437a083efc9e91c8b5dfeb7cffd
geraldiors <geralmers01@gmail.com>
2021-09-24 01:39:00 +0800

    fixing configs and main.go

--- configs/config.go
+++ configs/config.go
@@ -10,17 +10,14 @@ import (
 
 var DB *gorm.DB
 
-const (
-	mysql_dsn = `root:diosql@tcp(127.0.0.1:3306)/cookly?charset=utf8mb4&parseTime=True&loc=Local`
-)
-
 func InitDB() {
-	dsn := mysql_dsn
+	dsn := "root:diosql@tcp(127.0.0.1:3306)/cookly?charset=utf8mb4&parseTime=True&loc=Local"
 	var err error
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("failed to connect to database" + err.Error())
 	}
+	Migration()
 }
 
 func Migration() {
--- routes/routes.go
+++ routes/routes.go
@@ -6,11 +6,13 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
-func InitRoutes() {
+func InitRoutes() *echo.Echo {
 	e := echo.New()
 	baseRoute := e.Group("/api/v1")
 
 	// Users
 	baseRoute.POST("/register", users.RegisterController)
 	baseRoute.POST("/login", users.LoginController)
+
+	return e
 }
