5602843b2549d0802d6a69692aeb4f7cd5a64e20
geraldiors <geralmers01@gmail.com>
2021-09-29 18:22:51 +0800

    fixing mvc

--- mvc/controllers/recipes/recipe_controller.go
+++ mvc/controllers/recipes/recipe_controller.go
@@ -9,13 +9,14 @@ import (
 
 	"github.com/labstack/echo/v4"
 	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
 )
 
 func GetRecipeByID(c echo.Context) error {
 	var recipeById recipes.Recipe
 
 	id, _ := strconv.Atoi(c.Param("id"))
-	result := configs.DB.Raw("SELECT *,AVG(rv.rating) AS rating FROM reviews rv INNER JOIN recipes rp ON rv.recipe_id = rp.id AND rp.id = ?", id).Preload("RecipeCategory").Preload("User").First(&recipeById, id)
+	result := configs.DB.Raw("SELECT *,AVG(rv.rating) AS rating FROM reviews rv INNER JOIN recipes rp ON rv.recipe_id = rp.id AND rp.id = ?", id).Preload(clause.Associations).First(&recipeById, id)
 	if result.Error != nil {
 		return c.JSON(http.StatusInternalServerError, responses.BaseResponse{
 			Code:    http.StatusInternalServerError,
@@ -42,7 +43,8 @@ func GetRecipeByID(c echo.Context) error {
 func GetRecipesController(c echo.Context) error {
 	recipes := []recipes.Recipe{}
 
-	result := configs.DB.Preload("RecipeCategory").Preload("User").Find(&recipes)
+	result := configs.DB.Raw("SELECT *,AVG(rv.rating) AS rating FROM reviews rv INNER JOIN recipes rp ON rv.recipe_id = rp.id WHERE rp.id GROUP BY rp.id").Preload(clause.Associations).Find(&recipes)
+	//result := configs.DB.Preload(clause.Associations).Find(&recipes)
 	if result.Error != nil {
 		if result.Error != gorm.ErrRecordNotFound {
 			return c.JSON(http.StatusInternalServerError, responses.BaseResponse{
@@ -71,6 +73,15 @@ func CreateRecipeController(c echo.Context) error {
 	recipes.RecipeCategoryID = createRecipe.RecipeCategoryID
 	result := configs.DB.Create(&recipes)
 	if result.Error != nil {
+		return c.JSON(http.StatusInternalServerError, responses.BaseResponse{
+			Code:    http.StatusInternalServerError,
+			Message: "Failed to create recipe",
+			Data:    nil,
+		})
+	}
+
+	join := configs.DB.Preload(clause.Associations).First(&recipes)
+	if join.Error != nil {
 		return c.JSON(http.StatusInternalServerError, responses.BaseResponse{
 			Code:    http.StatusInternalServerError,
 			Message: "Failed to create recipe",
